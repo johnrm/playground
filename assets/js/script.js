8e11978cd131ce5385ab0a00de2f2961f3329231
johnrm <notjohnrm@gmail.com>
2024-04-01 16:06:10 +0000

    Add tileArray

---
+++ assets/js/script.js
@@ -0,0 +1,51 @@
+const tileArray = [
+    {
+        number: 0,
+        name: "Dude",
+        icon: '<i class="fa- solid fa - person"></i>'
+    },
+    {
+        number: 1,
+        name: "Empty",
+        display: '<i class="fa - regular fa - square"></i>'
+    },
+    {
+        number: 2,
+        name: "Zenny",
+        display: '<i class="fa - brands fa - bitcoin"></i>'
+    },
+    {
+        number: 3,
+        name: "Bomb",
+        display: '<i class="fa - solid fa - bomb"></i>'
+    },
+    {
+        number: 4,
+        name: "Tunnel",
+        display: '<i class="fa - solid fa - archway"></i>'
+    },
+    {
+        number: 5,
+        name: "Goal",
+        display: '<i class="fa - solid fa - bullseye"></i>'
+    },
+    {
+        number: 11,
+        name: "Hidden Empty",
+        display: '<i class="fa - solid fa - square"></i>'
+    },
+    {
+        number: 12,
+        name: "Hidden Zenny",
+        display: '<i class="fa - solid fa - square"></i>'
+    },
+    {
+        number: 13,
+        name: "Hidden Bomb",
+        display: '<i class="fa - solid fa - square"></i>'
+    },
+    {
+        number: 14,
+        name: "Hidden Tunnel",
+        display: '<i class="fa - solid fa - square"></i>'
+    }]
